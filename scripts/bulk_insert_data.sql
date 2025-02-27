0ca1eeeaaec0168cb0e963cc5a588a6c2e984c40
Edward Miro <139551397+Edz1984@users.noreply.github.com>
2025-02-28 05:41:23 +0800

    Update bulk_insert_data.sql

--- scripts/bulk_insert_data.sql
+++ scripts/bulk_insert_data.sql
@@ -1,16 +1,18 @@
 CREATE OR ALTER PROCEDURE bronze.load_bronze AS
 BEGIN
-	PRINT '=============================='
-	PRINT 'Loading Bronze Layer'
-	PRINT '=============================='
+	PRINT '==============================';
+	PRINT 'Loading Bronze Layer';
+	PRINT '==============================';
 
 
-	PRINT '------------------------------'
-	PRINT 'Loading CRM Tables'
-	PRINT '------------------------------'
+	PRINT '------------------------------';
+	PRINT 'Loading CRM Tables';
+	PRINT '------------------------------';
 
-
+	PRINT '>> Truncating Table: bronze.crm_cust_info';
 	TRUNCATE TABLE bronze.crm_cust_info;
+
+	PRINT '>> Inserting Data Into: bronze.crm_cust_info';
 	BULK INSERT bronze.crm_cust_info
 	FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 	WITH  (
@@ -19,8 +21,10 @@ BEGIN
 		TABLOCK
 	);
 
-
+	PRINT '>> Truncating Table: bronze.crm_prd_info';
 	TRUNCATE TABLE bronze.crm_prd_info;
+
+	PRINT '>> Inserting Data Into: bronze.crm_prd_info';
 	BULK INSERT bronze.crm_prd_info
 	FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 	WITH  (
@@ -29,8 +33,10 @@ BEGIN
 		TABLOCK
 	);
 
-
+	PRINT '>> Truncating Table: bronze.crm_sales_details';
 	TRUNCATE TABLE bronze.crm_sales_details;
+
+	PRINT '>> Inserting Data Into: bronze.crm_sales_details';
 	BULK INSERT bronze.crm_sales_details
 	FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 	WITH  (
@@ -40,12 +46,14 @@ BEGIN
 	);
 
 
-	PRINT '------------------------------'
-	PRINT 'Loading ERP Tables'
-	PRINT '------------------------------'
+	PRINT '------------------------------';
+	PRINT 'Loading ERP Tables';
+	PRINT '------------------------------';
 
-
+	PRINT '>> Truncating Table: bronze.erp_cust_az12';
 	TRUNCATE TABLE bronze.erp_cust_az12;
+
+	PRINT '>> Inserting Data Into: bronze.erp_cust_az12';
 	BULK INSERT bronze.erp_cust_az12
 	FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 	WITH  (
@@ -54,8 +62,10 @@ BEGIN
 		TABLOCK
 	);
 
-
+	PRINT '>> Truncating Table: bronze.erp_loc_a101';
 	TRUNCATE TABLE bronze.erp_loc_a101;
+	
+	PRINT '>> Inserting Data Into: bronze.erp_loc_a101';
 	BULK INSERT bronze.erp_loc_a101
 	FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 	WITH  (
@@ -64,8 +74,10 @@ BEGIN
 		TABLOCK
 	);
 
-
+	PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
 	TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+
+	PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
 	BULK INSERT bronze.erp_px_cat_g1v2
 	FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 	WITH  (
