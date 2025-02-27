46a7f0746fd952f1d5674d440197f84a1938aa38
Edward Miro <139551397+Edz1984@users.noreply.github.com>
2025-02-28 05:29:07 +0800

    Update bulk_insert_data.sql

--- scripts/bulk_insert_data.sql
+++ scripts/bulk_insert_data.sql
@@ -1,5 +1,4 @@
 TRUNCATE TABLE bronze.crm_cust_info;
-
 BULK INSERT bronze.crm_cust_info
 FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
 WITH  (
@@ -8,13 +7,8 @@ WITH  (
 	TABLOCK
 );
 
-SELECT COUNT(*) 
-FROM bronze.crm_cust_info;
-
-
 
 TRUNCATE TABLE bronze.crm_prd_info;
-
 BULK INSERT bronze.crm_prd_info
 FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
 WITH  (
@@ -23,13 +17,8 @@ WITH  (
 	TABLOCK
 );
 
-SELECT COUNT(*) 
-FROM bronze.crm_prd_info;
-
-
 
 TRUNCATE TABLE bronze.crm_sales_details;
-
 BULK INSERT bronze.crm_sales_details
 FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
 WITH  (
@@ -38,12 +27,8 @@ WITH  (
 	TABLOCK
 );
 
-SELECT COUNT(*) 
-FROM bronze.crm_sales_details;
-
 
 TRUNCATE TABLE bronze.erp_cust_az12;
-
 BULK INSERT bronze.erp_cust_az12
 FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
 WITH  (
@@ -52,13 +37,8 @@ WITH  (
 	TABLOCK
 );
 
-SELECT COUNT(*) 
-FROM bronze.erp_cust_az12;
-
-
 
 TRUNCATE TABLE bronze.erp_loc_a101;
-
 BULK INSERT bronze.erp_loc_a101
 FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
 WITH  (
@@ -67,14 +47,8 @@ WITH  (
 	TABLOCK
 );
 
-SELECT COUNT(*) 
-FROM bronze.erp_loc_a101;
-
-
-
 
 TRUNCATE TABLE bronze.erp_px_cat_g1v2;
-
 BULK INSERT bronze.erp_px_cat_g1v2
 FROM 'C:\Users\Edward\Downloads\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
 WITH  (
@@ -82,8 +56,5 @@ WITH  (
 	FIELDTERMINATOR = ',',
 	TABLOCK
 );
-
-SELECT *
-FROM bronze.erp_px_cat_g1v2
 
 
