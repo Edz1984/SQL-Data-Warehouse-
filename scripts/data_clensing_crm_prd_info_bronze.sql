888bb9ac7f48569af18c35615c4ecf235b65b560
Edward Miro <139551397+Edz1984@users.noreply.github.com>
2025-02-28 19:32:00 +0800

    Update data_clensing_crm_prd_info_bronze.sql

--- scripts/data_clensing_crm_prd_info_bronze.sql
+++ scripts/data_clensing_crm_prd_info_bronze.sql
@@ -46,17 +46,22 @@ INSERT INTO silver.crm_prd_info (
 )
 SELECT 
 	prd_id,
-	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,
-	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+	REPLACE(SUBSTRING(prd_key, 1, 5), '-', '_') AS cat_id,  -- Derived Columns Extract category ID
+	SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,         -- Derived Columns Extract product key
 	prd_nm,
-	ISNULL(prd_cost, 0) AS prd_cost,
+	ISNULL(prd_cost, 0) AS prd_cost,                        -- Removed NULL values
 	CASE UPPER(TRIM(prd_line))
 		WHEN  'M' THEN 'Mountain'
 		WHEN  'R' THEN 'Road'
 		WHEN  'S' THEN 'Other Sales'
 		WHEN  'T' THEN 'Touring'
 		ELSE 'n/a'
-	END AS prd_line,
+	END AS prd_line,                                        -- Data Normailzation Map product line codes to descriptive values
 	CAST(prd_start_dt AS DATE) AS prd_start_dt,
-	CAST(LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt)-1 AS DATE) AS prd_end_dt
+	CAST(
+		LEAD(prd_start_dt) OVER (PARTITION BY prd_key ORDER BY prd_start_dt) - 1 
+		AS DATE) AS prd_end_dt                           -- Data Enrichment Calculate end date as one day less than next start date
 FROM bronze.crm_prd_info 
+
+SELECT *
+FROM silver.crm_prd_info
